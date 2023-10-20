a9406b0f2b4bd214d6035687c82bdd52e0d92a46
Dione <dioneousseynou03@gmail.com>
2023-10-20 14:35:25 +0100

    first commit update

--- src/test/scala/oms/MockEstockLoadTest.scala
+++ src/test/scala/oms/MockEstockLoadTest.scala
@@ -1,10 +1,12 @@
+package OmsEstockLoad
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
-class StockSimulation extends Simulation {
+class EstockLoadTest extends Simulation  {
+
   val baseURL = "http://10.8.9.33:9308/oms-external-mock/eStock/rest/v1/stocks/article"
 
   val httpConf = http
@@ -13,7 +15,7 @@ class StockSimulation extends Simulation {
     .doNotTrackHeader("1")
 
 
-  val code: Iterator[String] = Iterator.from(1).map(i => f"${(i-1) % 50000 + 1}%05d")
+  val code: Iterator[String] = Iterator.from(1).map(i => f"${(i - 1) % 50000 + 1}%05d")
 
 
   val createArticleScenario = scenario("Create Article Scenario")
@@ -21,38 +23,33 @@ class StockSimulation extends Simulation {
       val code_uga: String = code.next()
       session.set("code_uga", code_uga)
     }
+    .pace(3000)
     .exec { session =>
       println("code_uga : " + session("code_uga").as[String])
       session
     }
+    .pause(10 seconds)
+    .pace(3000)
     .exec(
       http("Create Article")
-        .put("/do10000024-#code_uga/100")
+        .put("/do10000024-${code_uga}/100")
+
     )
-
-  val modifyStockScenario = scenario("Modify Stock Scenario")
-  .repeat(5){
-    exec(
-      http("Modify Stock")
-        .put("/do10000024-#code_uga/10")
-    )
-  }
-
-
-  val deleteArticleScenario = scenario("Delete Article Scenario")
+    .pause(10 seconds)
+    .repeat(5) {
+      exec(
+        http("Modify Stock")
+          .put("/do10000024-${code_uga}/10")
+      )
+    }
+    .pace(3000)
     .exec(
       http("Delete Article")
-        .put("/do10000024-#code_uga/0")
+        .put("/do10000024-${code_uga}/0")
     )
-
+    .pause(10 seconds)
   setUp(
-    createArticleScenario.inject(atOnceUsers(1)
-    ),
-    modifyStockScenario.inject(
-      atOnceUsers(1)
-    ),
-    deleteArticleScenario.inject(
-      atOnceUsers(1)
-    )
+    createArticleScenario.inject(rampUsers(50000) during(20 minutes))
   ).protocols(httpConf)
+
 }
